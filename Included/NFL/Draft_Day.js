5504ffa4446971816cb19e5a751a147888826088
Patrick_Hennecke <54962646+PatrickHennecke@users.noreply.github.com>
2022-05-31 16:59:36 -0500

    Delete Included/NFL directory

--- Included/NFL/Draft_Day.js
+++
@@ -1,9 +0,0 @@
-let currentYear = new Date();
-let Welcome = "Welcome to " + currentYear.getFullYear() + " Hennecke NFL Team Extravaganza";
-document.getElementById("Year").textContent = Welcome;
-
-$(document).ready(function() {
-    $('#b1, #b2, #b3, #b4, #b5, #b6, #b7, #b8, #b9, #b10, #b11, #b12, #b13, #b14, #b15, #b16, #b17, #b18, #b19, #b20, #b21, #b22, #b23, #b24, #b25, #b26, #b27, #b28, #b29, #b30, #b31, #b32').on('click', function() {
-        $(this).hidden();
-    });
-});
