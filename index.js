de6212c5a5aa7e082cf23806f1ddf483b40c2761
Patrick_Hennecke <54962646+PatrickHennecke@users.noreply.github.com>
2022-05-31 17:00:35 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+var scroll = new SmoothScroll('.navbar a[href*="#"]', {
+    customEasing: function (time) {
+
+		// return <your formulate with time as a multiplier>
+
+		// Example: easeInOut Quad
+		return time < 0.5 ? 2 * time * time : -1 + (4 - 2 * time) * time;
+
+	}
+});
