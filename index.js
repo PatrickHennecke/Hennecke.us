a0df4ad928492e3e51b5e20a5752b2a947ee7b29
Patrick_Hennecke <54962646+PatrickHennecke@users.noreply.github.com>
2022-06-03 18:04:37 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -12,3 +12,32 @@ var scroll = new SmoothScroll('.navbar a[href*="#"]', {
 AOS.init({
     disable: 'mobile'
   });
+
+  const container = document.getElementById("horizontal");
+  // where "container" is the id of the container
+	container.addEventListener("wheel", function (e) {
+	  if (e.deltaY > 0) {
+		container.scrollLeft += 100;
+		e.preventDefault();
+  // prevenDefault() will help avoid worrisome 
+  // inclusion of vertical scroll 
+	  }
+	  else {
+		container.scrollLeft -= 100;
+		e.preventDefault();
+	  }
+	});
+
+
+import barba from '@barba/core';
+import barbaCss from '@barba/css';
+
+barba.use(barbaCss);
+
+barba.init({
+    transitions: [
+        {
+            once(){}
+        }
+    ]
+});
